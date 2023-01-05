b8c1c5dea63624b781fad909305540aed456d901
Vivin V Abraham <vivin.abraham@experionglobal.com>
2023-01-05 17:01:38 +0530

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { Typography, AppBar, CssBaseline, Grid } from '@mui/material';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import './App.css';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { useState } from 'react'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-function App() {
+
+
+
+
+const App=()=> {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Fragment>
+      <CssBaseline />
+      <AppBar position='relative'>
+        <Toolbar>
+          <AddShoppingCartIcon />
+          <Typography variant='h6'>
+            Ecommerce
+          </Typography>
+
+          <Grid container justifyContent="flex-end">
+            <Button color='inherit' id='resource-menu' onClick={handleClick} aria-controls={open ? 'resource-menu' : undefined}
+              aria-haspopup='true'
+              aria-aria-expanded={open ? 'true' : undefined}><AccountCircleIcon />
+            </Button>
+          </Grid>
+
+
+          <Menu id='resource-menu' anchorEl={anchorEl} open={open} MenuListProps={{ 'aria-labelledby': 'resource-button', }} onClose={handleClose}>
+            <MenuItem onClick={handleClose}>My Profile</MenuItem>
+            <MenuItem onClick={handleClose}>Wishlist</MenuItem>
+            <MenuItem onClick={handleClose}>Cart</MenuItem>
+            <MenuItem onClick={handleClose}>Login</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    </Fragment>
   );
 }
 
